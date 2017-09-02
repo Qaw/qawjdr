ad0b93079d06e332f8d45b1b2d3f6fff860b5157
Christophe Laudon <ch.laudon@gmail.com>
2017-09-02 20:24:14 +0200

    Add DD5 Quickref with movements, actions and conditions in french

---
+++ dnd5e-quickref/js/data_action_fr.js
@@ -0,0 +1,190 @@
+data_action_fr = [
+    {
+        title: "Attaquer [Attack]",
+        icon: "crossed-swords",
+        subtitle: "Attaque de mêlée ou à distance",
+        description: "L'action la plus commune que vous pouvez faire en combat est l'action Attaquer, que ce soit en virevoltant avec une épée, en décochant une flèche ou avec ses propres poings.",
+        reference: "PHB, pgs. 192,194-195.",
+        bullets: [
+            "Certaines capacités, comme l'<i>Attaque supplémentaire</i> du guerrier, permettent de réaliser plus d'une attaque avec cette action. Chacune de ces attaques est un jet séparé et peut cibler une créature différente. Vous pouvez vous déplacer entre ces attaques",
+            "Lorsque vous utilisez l'action Attaquer et que vous attaquez avec une arme de corps à corps légère que vous portez dans une main, vous pouvez utiliser une action bonus pour attaquer avec une autre arme de corps à corps légère que vous portez dans l'autre main.",
+            "Vous pouvez remplacer une de vos attaques de mêlée avec une <i>Lutte</i> ou une <i>Bousculade</i>.",
+            "Certaines conditions donnent un avantage à l'attaque: attaques contre des cibles aveuglées, paralysées, pétrifiées, entravés, assommées ou inconscientes ;  melee attacks against prone targets; attacks by invisible or hidden attackers.",
+            "Certaines conditions donnent un désavantage à l'attaque: attaques contre des cibles invisibles ou cachées ; attaques à distance contre des cibles à terre ; attaques par des attaquants aveuglés, effrayés, empoisonnés ou entravés."
+        ]
+    },
+    {
+        title: "Lutter [Grapple]",
+        icon: "grab",
+        subtitle: "Attaque spéciale au corps à corps",
+        description: "Vous voulez attraper une créature ou lutter avec elle",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Vous pouvez utiliser l'action <i>Attaquer</i> pour faire une attaque au corps à corps spéciale, une lutte.",
+            "Si vous pouvez faire plusieurs attaques avec l'action Attaquer, cette action remplace l'une d'entre elles.",
+            "TLa cible de votre lutte ne doit pas être plus d'une taille supérieure à la vôtre, et elle doit être à votre portée.",
+            "En utilisant au moins une main de libre, vous essayez d'attraper la cible en réalisant un jet de lutte, qui est en fait un jet de Force (Athlétisme) en opposition à un jet de Force (Athlétisme) ou de Dextérité (Acrobaties) de l'ennemi (au choix de celui-ci).",
+            "Si vous réussissez, la cible est agrippée. Vous pouvez relâcher votre cible quand vous le voulez (cela ne nécessite pas d'action)."
+        ]
+    },
+    {
+        title: "Bousculer [Shove]",
+        icon: "hand",
+        subtitle: "Attaque spéciale au corps à corps",
+        description: "Bousculer une créature afin de la faire tomber à terre ou bien de la faire reculer.",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Vous pouvez utiliser l'action <i>Attaquer</i> pour bousculer une créature",
+            "Si vous pouvez faire plusieurs attaques avec l'action Attaquer, cette action remplace l'une d'entre elles.",
+            "La cible de cette action ne doit pas être plus d'une taille plus grande que vous et doit être à votre portée",
+            "Faites un jet de Force (Athlétisme) opposé à un jet de Force (Athlétisme) ou de Dextérité (Acrobaties) de la cible (au choix de celle-ci).",
+            "Si vous gagnez l'opposition, vous faites tomber à terre la cible ou bien la faites reculer de 1,50 mètre"
+
+        ]
+    },
+    {
+        title: "Lancer un sort [Cast a spell]",
+        icon: "magic-swirl",
+        subtitle: "Temps d'incantation de 1 action",
+        description: "Lancer un sort avec un temps d'incantation d'une action",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Vous ne pouvez pas lancer un sort avec votre action et un autre sort avec votre action bonus dans le même tour, sauf si l'action est utiliser pour lancer un sort mineur.",
+            "La cible d’un sort doit être dans la portée du sort.",
+            "Pour cibler quelque chose, vous devez avoir une ligne de vue dégagée.",
+            "Les sorts avec des composants matériels ne consomment pas le composant sauf si c'est explicitement précisé.",
+            "A moins que le coût du matériel ne soit donné, vous pouvez assumer que le coût reste négligeable et que le composant est simplement disponible dans le sac à composants",
+            "Certains sorts requierent que vous mainteniez votre concentration pour garder la magie active. Si vous perdez la concentratin, le sort s'arrête.",
+            "Vous perdez la concentration du sort si vous lancez un autre sort qui requiert de la concentration ou si vous êtes incapacité. A chaque fois que vous prenez des dégêts, vous devez faire un jet de sauvegarde de Constitution pour maintenir votre concentration. Le DC est égal au maximum entre 10 et la moitiée des dégâts pris."
+        ]
+    },
+    {
+        title: "Foncer [Dash]",
+        icon: "sprint",
+        subtitle: "Double la vitesse de déplacement",
+        description: "Quand vous entreprenez l'action Foncer, vous gagnez un mouvement supplémentaire pour le tour en cours.",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "La distance supplémentaire est égale à votre vitesse à laquelle on ajoute vos éventuels modificateurs."
+        ]
+    },
+    {
+        title: "Se désengager [Disengage]",
+        icon: "journey",
+        subtitle: "Empêche les attaques d'opportunité",
+        description: "Si vous vous Désengagez, votre mouvement ne provoque pas d'attaques d'opportunités pour le reste du tour.",
+        reference: "PHB, pg. 192.",
+        bullets: [
+        ]
+    },
+    {
+        title: "Esquiver [Dodge]",
+        icon: "aura",
+        subtitle: "Augmente la défense",
+        description: "Vous vous concentrez uniquement sur le fait d'éviter les attaques",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Jusqu'au début de votre prochain tour, chaque jet d'attaque lancé contre vous à un désavantage si vous voyez l'attaquant, et vos jets de sauvegarde de Dextérité ont l'avantage.",
+            "Vous perdez cet avantage si vous êtes incapable d'agir ou si votre vitesse tombe à 0."
+        ]
+    },
+    {
+        title: "S'échapper d'une lutte",
+        icon: "manacles",
+        subtitle: "S'échapper d'une lutte",
+        description: "Une créature agrippée peut utiliser son action pour s'échapper.",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Pour cela, elle doit réussir un jet de Force (Athlétisme) ou de Dextérité (Acrobaties) en opposition à votre jet de Force (Athlétisme)."
+        ]
+    },
+    {
+        title: "Aider [Help]",
+        icon: "telepathy",
+        subtitle: "Donne un avantage à un allié",
+        description: "Vous pouvez aider d'autres créatures dans l'accomplissement d'une tâche.",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "La créature que vous aidez a l'avantage au prochain jet de caractéristique qu'elle fera pour accomplir la tâche pour laquelle vous l'aidez, à condition que le jet soit effectué avant le début de votre prochain tour.",
+            "Vous pouvez aussi Aider une créature amicale à attaquer une créature qui est dans un rayon de 1,50 mètre autour de vous. ",
+            "Si votre allié attaque la cible avant votre prochain tour, le premier jet d'attaque qu'il fait a l'avantage."
+        ]
+    },
+    {
+        title: "Utiliser un objet [Use an object]",
+        icon: "snatch",
+        subtitle: "Interact, use special abilities",
+        description: "Interact with a second object or use special object abilities",
+        reference: "PHB, pg. 193.",
+        bullets: [
+            "Vous pouvez interagir avec un objet gratuitement pendant votre tour (comme dégainer une arme ou ouvrir une porte). Si vous voulez interagir avec un deuxième objet, utilisez cette action."
+        ]
+    },
+    {
+        title: "Utiliser un bouclier",
+        icon: "round-shield",
+        subtitle: "Mettre ou oter un bouclier",
+        description: "Mettre ou ôter un bouclier",
+        reference: "PHB, pgs. 144-146.",
+        bullets: [
+            "Mettre ou ôter un bouclier prend une action.",
+            "Mettre ou ôter une armure prend plusieurs minutes.<table style='text-align:left'><tr><th>Catégorie</th><th>Mettre</th><th>Ôter</th></tr><tr><td>Armure légère</td><td>1 minute</td><td>1 minute</td></tr><tr><td>Armure intermédiaire</td><td>5 minutes</td><td>1 minute</td></tr><tr><td>Armure lourde</td><td>10 minutes</td><td>5 minutes</td></tr></table>"
+        ]
+    },
+    {
+        title: "Se cacher [Hide]",
+        icon: "hood",
+        subtitle: "",
+        description: "Essayer de se cacher",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Vous ne pouvez pas vous cacher d'une créature qui peut vous voir clairement. Vous devez avoir une couverture totale, être dans une zone très sombre, être invisible, ou sinon que la vision de l'ennemi soit bloquée.",
+            "vous révélez votre position si vous faites du bruit, comme crier ou renverser un vase.",
+            "Lorsque vous essayez de vous cacher, faites un jet de Dextérité (Discrétion). Tant que vous n'êtes pas découvert ou que vous n'arrêtez pas de vous cacher, le résultat de ce jet est celui qui sera pris en compte pour une opposition avec le jet de Sagesse (Perception) de toute créature qui recherche activement des signes de votre présence.",
+            "Une créature vous remarque même si elle ne vous cherche pas sauf si votre jet de Discrétion est supérieur à sa Perception Passive (10 + Modificateur de sagesse + Maîtrise éventuelle ; +5 si avantage, -5 si désavantage).",
+            "En dehors du combat, vous pouvez faire un jet de Dextérité (Discrétion) pour des actions comme se cacher des ennemis, éviter des gardes, s'éclipser sans être remarqué ou se faufiler sans être vu ou entendu."
+        ]
+    },
+    {
+        title: "Chercher",
+        icon: "magnifying-glass",
+        subtitle: "Essayer de trouver quelque chose",
+        description: "",
+        reference: "PHB, pg. 193.",
+        bullets: [
+            "Selon la nature de la recherche, le MJ peut vous demander un jet de Sagesse (Perception) ou Intelligence (Investigation)."
+        ]
+    },
+    {
+        title: "Se tenir prêt [Ready]",
+        icon: "stopwatch",
+        subtitle: "Attendre avant d'agir",
+        description: "Parfois, vous voulez agir juste avant un ennemi ou attendre une circonstance particulière avant d'agir.",
+        reference: "PHB, pg. 193.",
+        bullets: [
+            "En premier lieu, vous devez décider quelle circonstance perceptible déclenchera votre réaction.",
+            "Puis choisir l'action ou le déplacement qui aura lieu lorsqu'elle sera déclenchée.",
+            "Quand le déclencheur est activé, vous pouvez soit utiliser votre réaction dès qu'il se termine, soit l'ignorer. Souvenez-vous que vous n'avez droit qu'à une seule réaction par tour.",
+            "Quand vous préparez un sort, vous le lancez normalement puis retenez son énergie, que vous ne relâchez par votre réaction que lorsque le déclencheur est activé. Pour être prêt, le sort doit avoir un temps d'incantation d'une action, et il est nécessaire d'être concentré pour contenir la magie du sort. Si votre concentration est interrompue, le sort se dissipe sans aucun effet."
+        ]
+    },
+    {
+        title: "Utiliser une capacité de classe",
+        icon: "embrassed-energy",
+        subtitle: "Certaines capacités utilisent une action",
+        description: "Utiliser une capacité de race ou de classe qui nécessite une action.",
+        reference: "Voir la capacité pour le détail.",
+        bullets: [
+        ]
+    },
+    {
+        title: "Improviser",
+        icon: "juggler",
+        subtitle: "Toute action absente de cette liste",
+        description: "Faire n'importe quelle action que vous imaginez",
+        reference: "PHB, pg. 193.",
+        bullets: [
+            "Quand vous décrivez une action non décrite dans les règles, le MJ vous dit si cette action est possible et quel genre de jet de dés détermine, si nécessaire, le succès ou l'échec."
+        ]
+    }
+];
---
+++ dnd5e-quickref/js/data_reaction_fr.js
@@ -0,0 +1,36 @@
+data_reaction_fr = [
+    {
+        title: "Attaque d'opportunité",
+        icon: "crossed-swords",
+        subtitle: "Un ennemi sort de votre portée",
+        description: "Vous pouvez rarement vous déplacer avec insouciance sans vous mettre en danger",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Déclencheur: un ennemi sort de votre portée.",
+            "Make one melee attack against the provoking creature.",
+            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
+            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
+        ]
+    },
+    {
+        title: "Readied action",
+        icon: "stopwatch",
+        subtitle: "Part of your Ready action",
+        description: "Execute the reaction specified by your Ready action",
+        reference: "PHB, pg. 193.",
+        bullets: [
+            "Trigger: specified by your <i>Ready</i> action."
+        ]
+    },
+    {
+        title: "Cast a spell",
+        icon: "magic-swirl",
+        subtitle: "Cast time of 1 reaction",
+        description: "Cast a spell with a casting time of 1 reaction",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Trigger: specified by the spell.",
+            "For further details, see the <i>Cast a spell</i> action."
+        ]
+    }
+]
