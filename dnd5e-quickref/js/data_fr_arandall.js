72259aec85d23af6a9f540bb8b090371757ecf18
Christophe Laudon <ch.laudon@gmail.com>
2017-09-06 21:36:03 +0200

    Add Arandall quickref

---
+++ dnd5e-quickref/js/data_fr_arandall.js
@@ -0,0 +1,37 @@
+data_bonusaction_fr_arandall_heroic = [
+    {
+        title: "Frappe rayonnante",
+        icon: "flying-dagger",
+        subtitle: "Cause des dégâts rayonnants en mêlée",
+        description: "Cause des dommages rayonnants à la cible",
+        reference: "Voie héroïque du Guérisseur",
+        bullets: [
+            "Au lieu de lancer un sort de Soins, vous faites des dégâts rayonnants à la cible en plus des dommages de l'arme.",
+            "Les dégâts sont de 2D8 pour un emplacement de niveau 1, plus 1D8 pour chaque niveau supérieur pour un maximum de 5D8.",
+            "Les dommages augmentent de 1D8 si la cible est un mort-vivant ou un démon."
+        ]
+    },
+    {
+        title: "Sorts du Guérisseur",
+        icon: "magic-swirl",
+        subtitle: "Lancer les sorts innés avec une action bonus",
+        description: "Les sorts innés de la voie héroïque avec un temps d'incantation d'une action bonus",
+        reference: "Voie héroïque du Guérisseur",
+        bullets: [
+            "<table><tr><th>Niveau</th><th style='text-align:center'>Sort</th><th>Ecole</th><th>Fréquence</th></tr><tr><td>1</td><td>Mot de guérison</td><td>Evocation</td><td>2 par repos long</td></tr></table>"
+        ]
+    }
+];
+
+data_action_fr_arandall_heroic = [
+    {
+        title: "Sorts du Guérisseur",
+        icon: "magic-swirl",
+        subtitle: "Lancer les sorts innés avec une action",
+        description: "Les sorts innés de la voie héroïque avec un temps d'incantation d'une action",
+        reference: "Voie héroïque du Guérisseur",
+        bullets: [
+            "<table><tr><th>Niveau</th><th style='text-align:center'>Sort</th><th>Ecole</th><th>Fréquence</th></tr><tr><td>0</td><td>Stabilisation</td><td>Nécromancie</td><td>A volonté</td></tr><tr><td>1</td><td>Soins</td><td>Evocation</td><td>5 par repos long</td></tr><tr><td>2</td><td>Protection contre le poison</td><td>Abjuration</td><td>5 par repos long</td></tr><tr><td>2</td><td>Restauration partielle</td><td>Abjuration</td><td>3 par repos long</td></tr><tr><td>4</td><td>Aura de vie</td><td>Abjuration</td><td>1 par repos long</td></tr></table>"
+        ]
+    }
+];
---
+++ dnd5e-quickref/js/quickref_fr_arandall.js
@@ -0,0 +1,10 @@
+function initPerso() {
+
+    fill_section(data_action_fr_arandall_heroic, "basic-actions", "Action", "heroic");
+    fill_section(data_bonusaction_fr_arandall_heroic, "basic-bonus-actions", "Bonus action", "heroic");
+
+    var modal = document.getElementById("modal");
+    modal.onclick = hide_modal;
+}
+
+$(window).load(initPerso);
