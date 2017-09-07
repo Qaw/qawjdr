261041fb9bfdd437e51a3d37edbe8ac739ec250a
Christophe Laudon <ch.laudon@gmail.com>
2017-09-08 00:13:33 +0200

    Add Farlans quickref

---
+++ dnd5e-quickref/js/data_fr_farlans.js
@@ -0,0 +1,132 @@
+data_character_farlans =  [
+    {
+        bullets: [
+            "Votre maximum de KI est 14 + Modificateur Constitution. Vous le reconstituez après un repos court ou un repos long. JS d'un adversaire contre KI : 8 + 5 + Modificateur Force",
+            "2 attaques par round",
+            "Classe d'armure : bonus de +5 si vous êtes en armure légère, même si vous êtes surpris. Vous avez encore un bonus de +2 si vous êtes en mouvement.",
+            "Mouvement : 19,5m (65')",
+            "Avantage au jet d'initiative. Si vous dépensez 2 points de Ki, même si vous êtes surpris, vous pouvez agir normalement.",
+            "Pour un jet de Lutte vous pouvez utiliser Dextérité (Acrobaties) au lieu de Force (Athlétisme). Vous avez en plus l'avantage à votre jet de Lutte.",
+            "Evasion : JS de Dextérité pour aucun dommage, et demi-dommage si vous ratez.",
+            "JS Dextérité : +10 + Mod Dextérité ; JS Force : +5 + Mod Force ; JS Autres : +3 + Mod Caractéristique"
+        ]
+    }
+];
+
+data_bonusaction_fr_farlans_class = [
+    {
+        title: "Frappe à mains nues",
+        icon: "fist",
+        subtitle: "Attaque à mains nues supplémentaire",
+        description: "Vous pouvez faire une attaque à mains nues en plus de votre action Attaque si c'était une attaque à mains nues ou avec une arme de défenseur.",
+        reference: "Défenseur Coup de maître",
+        bullets: [
+        ]
+    },
+    {
+        title: "Pas du Vent [Step of the Wind]",
+        icon: "fist",
+        subtitle: "1 Ki pour se désengager ou foncer",
+        description: "",
+        reference: "Défenseur Focus interne",
+        bullets: [
+            "1 point de Ki pour une action Se désengager ou Foncer.",
+            "Votre distance de saut est doublée."
+        ]
+    },
+    {
+        title: "Défense Patiente",
+        icon: "fist",
+        subtitle: "1 Ki pour Esquiver",
+        description: "",
+        reference: "Défenseur Focus interne ",
+        bullets: [
+            "1 point de Ki pour une action Esquiver."
+        ]
+    },
+    {
+        title: "Rafale de coups",
+        icon: "fist",
+        subtitle: "1 Ki pour attaques à mains nues",
+        description: "",
+        reference: "Défenseur Focus interne",
+        bullets: [
+            "1 point de Ki pour deux attaques à mains nues après votre action d'Attaque."
+        ]
+    },
+    {
+        title: "Résilience",
+        icon: "fist",
+        subtitle: "5 PV temporaires par point de KI",
+        description: "",
+        reference: "Défenseur",
+        bullets: [
+            "Utiliser jusqu'à 5 points de Ki pour gagner des PV temporaires : 5 par point de Ki."
+        ]
+    }
+];
+
+data_action_fr_farlans_class = [
+    {
+        title: "Attaque à mains nues",
+        icon: "fist",
+        subtitle: "Attaque à mains nues du Défenseur",
+        description: "",
+        reference: "Défenseur et Défenseur archétype Vengeur",
+        bullets: [
+            "Utiliser <b>la dextérité</b> au lieu de la force pour le jet d'attaque et de dommage. Les dommages sont de <b>1D4 + 1D8</b>",
+            "<b>Assommer</b> (coût : 1 Ki) : la cible doit faire un JS Constitution ou être assommée jusqu'à la fin de votre prochain tour.",
+            "<b>Rendre aveugle ou sourd</b> (coût : 1 Ki): si la cible a été assommée, elle doit faire un JS Constitution ou être soit aveugle soit sourde pendant Modificateur Force rounds.",
+            "<b>Faire tomber</b> (coût : 1 Ki) : la cible doit réussir JS Dextérité ou tomber à plat ventre. La cible a l'avantage au JS si elle est de taille Large, elle est immunisée si elle est de taille Enorme ou plus.",
+            "<b>Repousser</b> (coût : 1 Ki) : la cible doit réussir JS Force ou reculer de 4,5m. La cible a l'avantage au JS si elle est de taille Large, elle est immunisée si elle est de taille Enorme ou plus.",
+            "<b>Coup précis</b> (coût : 1 Ki) : vos attaques sont considérées comme magique pour vaincre la résistance ou l'immunité aux attaques non-magiques. L'effet dure jusqu'au début de votre prochain tour."
+        ]
+    }
+];
+
+data_action_fr_farlans_heroic = [
+    {
+        title: "Attaques multiples",
+        icon: "fist",
+        subtitle: "Attaquer tous ceux à portée (1 / repos court)",
+        description: "",
+        reference: "Défenseur",
+        bullets: [
+            "Vous pouvez utiliser votre action de combat pour attaquer toutes les créatures dans un rayon de 1,5m",
+            "Vous devez faire un jet séparé pour chaque cible"
+        ]
+    }
+];
+
+data_bonusaction_fr_farlans_heroic = [
+    {
+        title: "Foncer",
+        icon: "sprint",
+        subtitle: "Double la vitesse de déplacement",
+        description: "Voir l'action Foncer",
+        reference: "Voie héroïque du Vif",
+        bullets: [
+        ]
+    },
+    {
+        title: "Esquiver",
+        icon: "aura",
+        subtitle: "Augmente la défense",
+        description: "Voir l'action Esquiver",
+        reference: "Voie héroïque du Vif",
+        bullets: [
+        ]
+    },
+    {
+        title: "Explosion de vitesse",
+        icon: "fist",
+        subtitle: "Action supplémentaire (3 / repos long)",
+        description: "",
+        reference: "Voie héroïque du Vif",
+        bullets: [
+            "Vous gagnez une action additionnelle à chacun de vos tours : cette action peut être Attaquer, Foncer, Se désengager, Se cacher ou utiliser un objet",
+            "La durée est de 1 minute ou jusqu'à ce que vous perdiez la concentration. A la fin vous gagnez un niveau de fatigue."
+        ]
+    }
+];
+
---
+++ dnd5e-quickref/js/quickref_fr_farlans.js
@@ -0,0 +1,15 @@
+function initPerso() {
+
+    fill_character_section(data_character_farlans,"character");
+    
+    fill_section(data_bonusaction_fr_farlans_class, "basic-bonus-actions", "Bonus action", "class");
+    fill_section(data_action_fr_farlans_class, "basic-actions", "Action", "class");
+
+    fill_section(data_action_fr_farlans_heroic, "basic-actions", "Action", "heroic");
+    fill_section(data_bonusaction_fr_farlans_heroic, "basic-bonus-actions", "Bonus action", "heroic");
+
+    var modal = document.getElementById("modal");
+    modal.onclick = hide_modal;
+}
+
+$(window).load(initPerso);
