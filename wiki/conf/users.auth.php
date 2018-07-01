2510692e8278fc27215cdc7f28bff891139ecaf5
Christophe Laudon <ch.laudon@gmail.com>
2018-07-01 21:59:02 +0200

    :sparkles: Save the wiki files

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,16 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+adminqaw:$1$C4eSLkjB$s8A.98PUaWN/11/5qyzfE.:Christophe:ch.laudon@gmail.com:admin,user
+dom:$1$pu9rCsTx$elMnLiDNa.cmAej2qwJCH/:Dom:servenn@hotmail.com:user
+lionel:$1$GspNrVBV$wp19rM.FwXrVXci8yeiM.1:Lionel:lionelfleuret@gmail.com:user
+rico:$1$WjLbqk6r$iVEbGrdtVlGuHuuJjy/sz/:Richard:rtribouilloy@free.fr:user
+yann:$1$jC6kBXT6$Imjh7hZJ4L4l9V0jrOItJ0:Yann:augayann@yahoo.fr:user
