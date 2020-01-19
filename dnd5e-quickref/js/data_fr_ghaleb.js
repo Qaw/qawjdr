60b7fed4bd96f67b5e22531001db71b42deeb7a8
Christophe Laudon <ch.laudon@gmail.com>
2020-01-19 16:06:52 +0100

    Add ghaleb's quickref and maneuvers's section

---
+++ dnd5e-quickref/js/data_fr_ghaleb.js
@@ -0,0 +1,149 @@
+data_action_fr_ghaleb_class = [
+    {
+        title: "Sursaut",
+        icon: "sword-array",
+        subtitle: "Réaliser une action supplémentaire",
+        description: "",
+        reference: "Guerrier",
+        bullets: [
+            "Vous pouvez réaliser une action supplémentaire en plus de votre action normale et de votre éventuelle action bonus.",
+            "Une fois cette capacité utilisée, vous devez terminer un repos court ou long pour pouvoir l'utiliser de nouveau."
+        ]
+    }
+];
+
+data_bonusaction_fr_ghaleb_class = [
+    {
+        title: "Second souffle",
+        icon: "strong",
+        subtitle: "Se soigner",
+        description: "Puiser dans son endurance pour se soigner",
+        reference: "Guerrier",
+        bullets: [
+            "Vous possédez une réserve d'endurance limitée dans laquelle vous pouvez puiser pour vous protéger contre les dégâts.", 
+            "À votre tour vous pouvez utiliser une action bonus pour regagner un nombre de pv égal à 1d10 + 14 (votre niveau de guerrier).",
+            "Une fois cette capacité utilisée, vous devez terminer un repos court ou long pour pouvoir l'utiliser de nouveau."
+        ]
+    },
+    {
+        title: "Shurapoli (Danse de la lame)",
+        icon: "strong",
+        subtitle: "",
+        description: "Puiser dans son endurance pour se soigner",
+        reference: "Danseur de lame pellurien",
+        bullets: [
+            "Si vous portez une armure légère ou aucune armure, vous pouvez utiliser une action bonus pour entrer en shurapoli", 
+            "Vous gagnerz les avantages suivants. Quand vous portez une épée à deux mains, vous pouvez utiliser votre Dextérité au lieu de votre Force pour les jets d'attaque et de dégâts.",
+            "Vous pouvez ajouter la moitié de votre bonus de compétence à votre CA contre toutes les attaques.",
+            "La danse s'arrête si votre vitesse tombe à 0."
+        ]
+    }
+];
+
+data_reaction_fr_ghaleb_heroic = [
+    {
+        title: "Parade",
+        icon: "sword-clash",
+        subtitle: "Parer une attaque réussie",
+        description: "",
+        reference: "Guerrier",
+        bullets: [ 
+            "Si une créature vous touche avec une attaque au corps à corps, vous pouvez utiliser votre réaction et un dé de supériorité pour réduire les dégâts d'un montant égal à votre résultat au dé de supériorité + votre modificateur de Dextérité."
+        ]
+    }
+];
+
+data_maneuver_fr_ghaleb_class = [
+    {
+        title: "Vagues constantes",
+        icon: "run",
+        subtitle: "Améliorer l'acrobatie ou la dextérité",
+        description: "",
+        reference: "Danseur de lame pellurien",
+        bullets: [
+            "Lorsque vous faites un jet qui vous permet d'appliquer votre compétence en acrobatie ou lorsque vous faites une sauvegarde de dextérité, vous pouvez utiliser un dé de supériorité pour augmenter le jet.",
+            "Ajoutez la moitié (arrondi au supérieur) du nombre obtenu sur le dé de supériorité à votre jet. Vous appliquez ce bonus après avoir fait le test mais avant de savoir s'il a réussi..."
+        ]
+    },
+    {
+        title: "Désarmement",
+        icon: "sword-in-stone",
+        subtitle: "Désarmer avec une attaque réussie",
+        description: "",
+        reference: "Guerrier",
+        bullets: [
+            "Si vous touchez une créature lors d'une attaque avec une arme, vous pouvez dépenser un dé de supériorité pour tenter de désarmer la cible, la forçant à lâcher un objet de votre choix qu’elle a en main.", 
+            "Vous ajoutez le dé de supériorité aux dégâts de l’attaque, et la cible doit faire un jet de sauvegarde de Force. En cas d’échec, elle lâche l’objet que vous avez désigné. L’objet tombe à ses pieds."
+        ]
+    },
+    {
+        title: "Jeu de jambes défensif",
+        icon: "tread",
+        subtitle: "Se déplacer défensivement",
+        description: "",
+        reference: "Guerrier",
+        bullets: [
+            "Si vous effectuez un déplacement, vous pouvez dépenser un dé de supériorité, jeter le dé et ajouter le résultat à votre CA jusqu'à la fin de votre déplacement."
+        ]
+    },
+    {
+        title: "Attaque précise",
+        icon: "targeting",
+        subtitle: "Améliorer le jet d'attaque",
+        description: "",
+        reference: "Guerrier",
+        bullets: [
+            "Si vous réalisez une attaque avec une arme, vous pouvez dépenser un dé de supériorité et l’ajouter au jet d'attaque.",
+            "Vous pouvez utiliser cette manoeuvre avant ou après avoir lancé le jet d'attaque, mais avant qu'un quelconque effet de l'attaque ne s'applique."
+        ]
+    }
+
+    
+];
+
+data_maneuver_fr_ghaleb_heroic = [
+    {
+        title: "Riposte",
+        icon: "saber-slash",
+        subtitle: "Riposter contre une attaque ratée",
+        description: "",
+        reference: "Guerrier",
+        bullets: [ 
+            "Si une créature vous rate lors d'une attaque au corps à corps, vous pouvez utiliser votre réaction et un dé de supériorité pour faire une attaque avec une arme au corps à corps contre cette créature.",
+            "Si vous touchez, ajoutez votre dé de supériorité aux dégâts."
+        ]
+    },
+    {
+        title: "Fente",
+        icon: "all-for-one",
+        subtitle: "Augmenter l'allonge d'une attaque",
+        description: "",
+        reference: "Guerrier",
+        bullets: [ 
+            "Si vous réalisez une attaque avec une arme au corps à corps durant votre tour, vous pouvez dépenser un dé de supériorité pour augmenter l'allonge de votre frappe de 1,50 mètre. ",
+            "Si vous touchez, ajoutez votre dé de supériorité aux dégâts."
+        ]
+    },
+    {
+        title: "Distraction",
+        icon: "sword-spin",
+        subtitle: "Distraire avec une attaque réussie",
+        description: "",
+        reference: "Guerrier",
+        bullets: [ 
+            "Si vous touchez une créature lors d'une attaque avec une arme, vous pouvez dépenser un dé de supériorité pour distraire la créature et provoquer une ouverture pour vos alliés.",
+            "Ajoutez votre dé de supériorité à vos dégâts. La prochaine attaque contre votre cible aura l'avantage si ce n’est pas vous qui frappez et si elle est faite avant le début de votre prochain tour."
+        ]
+    },
+    {
+        title: "Attaque de manoeuvre.",
+        icon: "backup",
+        subtitle: "Manoeuvrer un allié avec une attaque réussie",
+        description: "",
+        reference: "Guerrier",
+        bullets: [ 
+            "Si vous touchez une créature lors d'une attaque avec une arme, vous pouvez dépenser un dé de supériorité pour placer l’un de vos alliés dans une position avantageuse.",
+            "Ajoutez votre dé de supériorité aux dégâts et choisissez une créature amicale qui peut vous voir ou vous entendre. Cette créature pourra utiliser sa réaction pour se déplacer de la moitié de sa vitesse sans provoquer d'attaque d’opportunité de la part de la cible de votre attaque."
+        ]
+    }      
+];
